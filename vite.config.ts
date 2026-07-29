b42b06b85614ee76233f6d1fa16822f8e1cea51e
tmanoj2007 <tummalapallimanoj7@gmail.com>
2026-07-29 20:15:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/manojgemini/", // <-- Right here! The slashes go at the start and end of your repo name.
+  base: "/manojgemini/",
   plugins: [react()],
 })
