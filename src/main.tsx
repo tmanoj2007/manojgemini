eb6a42e907fac3504c9e2f09d79d85af442b39df
tmanoj2007 <tummalapallimanoj7@gmail.com>
2026-07-30 22:51:45 +0530

    Use relative base path for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css' // <--- Make sure this line is here!
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default defineConfig({
+  base: './',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
